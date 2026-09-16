41ea46b38e99d14f6caff67cdc5a2ca38104a921
Muskan Tandel <muskantandel824@gmail.com>
2026-09-16 23:00:04 +0530

    v1

--- terraform/security.tf
+++ terraform/security.tf
@@ -8,14 +8,14 @@ resource "aws_security_group" "app" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
+    cidr_blocks = ["${var.my_ip}/32"]
   }
 
   ingress {
     description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
